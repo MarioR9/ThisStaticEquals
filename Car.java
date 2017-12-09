971ccebdaae410e6480aa208016b09465754737c
MarioR9 <mr20620@email.vccs.edu>
2017-12-09 11:13:34 -0500

    First commit

---
+++ Car.java
@@ -0,0 +1,74 @@
+
+public class Car {
+    private String color;
+    private int horsepower;
+    private double enginesize;
+    private String make;
+    private static int count;
+
+    public Car() {
+        color = "Pearl White";
+        horsepower = 180;
+        enginesize = 6.1;
+        make = "Mitsubishi";
+        count++;
+    }
+
+    public Car(String color, int horsepower, double enginesize, String make) {
+        this.color = color;
+        this.horsepower = horsepower;
+        this.enginesize = enginesize;
+        this.make = make;
+        count++;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getHorsepower() {
+        return horsepower;
+    }
+
+    public void setHorsepower(int horsepower) {
+        this.horsepower = horsepower;
+    }
+
+    public double getEnginesize() {
+        return enginesize;
+    }
+
+    public void setEnginesize(double enginesize) {
+        this.enginesize = enginesize;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    public boolean equals(Car car){
+        return this.color.equals(car.color) && this.horsepower == (car.horsepower) && this.enginesize == car.enginesize && car.make.equals(car.make);
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "color='" + color + '\'' +
+                ", horsepower=" + horsepower +
+                ", enginesize=" + enginesize +
+                ", make='" + make + '\'' +
+                '}';
+    }
+}
---
+++ Maindriver.java
@@ -0,0 +1,63 @@
+import javafx.application.Application;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextInputDialog;
+import javafx.stage.Stage;
+
+    public class Maindriver extends Application {
+        public void start (Stage primaryStage){
+            TextInputDialog input = new TextInputDialog();
+            Alert output = new Alert(Alert.AlertType.INFORMATION);
+            input.setHeaderText("Car Creation Center");
+            input.setContentText("Enter The Amount of cars");
+            input.showAndWait();
+            int numCompare;
+            int numCars = Integer.parseInt(input.getResult());
+            Car[] car = new Car[(numCars)];
+            int carNum=0;
+
+            for (int count = 1; count<=numCars; count++){
+
+                input.setHeaderText("Car Color");
+                input.setContentText("Enter the color for car " + count);
+                input.showAndWait();
+                String color = input.getResult();
+
+                input.setHeaderText("Car Horsepower");
+                input.setContentText("Enter the horsepower for car " + count);
+                input.showAndWait();
+                int horsepower = Integer.parseInt(input.getResult());
+
+                input.setHeaderText("Car Engine Size");
+                input.setContentText("Enter the engine size for car " + count);
+                input.showAndWait();
+                double enginesize = Double.parseDouble(input.getResult());
+
+                input.setHeaderText("Car Make");
+                input.setContentText("Enter the make for car " + count);
+                input.showAndWait();
+                String make = input.getResult();
+
+                car[carNum] = new Car(color,horsepower,enginesize,make);
+
+                output.setContentText(carNum+" "+car[carNum]);
+                output.showAndWait();
+                carNum++;
+
+            }
+
+            input.setHeaderText("Enter two cars to compare");
+            input.setContentText("Enter car number for the first car");
+            input.showAndWait();
+            numCompare = Integer.parseInt(input.getResult())-1;
+            input.setHeaderText("Enter two cars to compare");
+            input.setContentText("Enter car number for the second car");
+            input.showAndWait();
+            output.setHeaderText("Are the two cars equal?");
+            output.setContentText(""+car[numCompare].equals(car[Integer.parseInt(input.getResult())-1]));
+
+            output.showAndWait();
+            System.exit(0);
+
+        }
+
+    }
